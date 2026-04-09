2a650eed4a97138e473e31d922898aa6bedfa9d5
josephgakuru984-png <josephgakuru984@gmail.com>
2026-04-09 14:01:18 +0300

    initial commit

---
+++ app/src/main/java/com/josejt/travelar/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.josejt.travelar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.josejt.travelar.ui.theme.TravelarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TravelarTheme {
+                // 1. Create the NavController to manage screen switching
+                val navController = rememberNavController()
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // 2. Call your Main Navigation setup here
+                    // We will create AppNavigation in a separate step
+                    AppNavigation(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/josejt/travelar/ui/theme/screens/oboarding/onboardingscreen.kt
@@ -0,0 +1,2 @@
+package com.josejt.travelar.ui.theme.screens.oboarding
+
